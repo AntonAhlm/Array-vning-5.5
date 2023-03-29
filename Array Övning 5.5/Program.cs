566cbcfc0bbe8c0559d8690175662b6dfbc29ddf
Anton <>
2023-03-29 10:36:19 +0200

    Add project files.

---
+++ Array Övning 5.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Ålder = new int[99];
+            int i = 0;
+            string svar;
+            do
+            {
+                Console.Write("Skriv in åldern på en hyresgäst : ");
+                Ålder[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+
+                Console.Write("Vill du fortsätta? j/n : ");
+                svar=Console.ReadLine();
+
+            } while (svar=="j");
+
+
+            for (i = 0; i < Ålder.Length; i++)
+                if (Ålder[i] > 0)
+                {
+                    Console.WriteLine("Personen i index " + i + " är " + Ålder[i] + " år gammal");
+                }
+                else
+                    break;
+        }
+    }
+}
